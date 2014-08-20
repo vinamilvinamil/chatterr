f5f3cd6a70131f961a610fd2e23900b9f7d0694d
MHanslo <mikehanslo@gmail.com>
2014-08-21 00:21:11 +0200

    updated navbar : new logo

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -24,26 +24,32 @@
 
   <body>
 
-    <div class="navbar navbar-default navbar-fixed-top mc-nav" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div class="collapse navbar-collapse pull-right">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="{{ URL::to('/') }}">Home</a></li>
+  <nav class="navbar navbar-default mc-nav" role="navigation">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand logo" href="{{ url('/') }}"><img src="{{ asset('images/logo-s.png') }}" /></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-left">
+            <li><a href="{{ URL::to('/') }}">Home</a></li>
             <li><a href="{{ url('about') }}">About</a></li>
             <li><a href="{{ url('forum') }}">Try it</a></li>
             <li><a href="{{ url('contact') }}">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
+            </ul>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
 
     
 
