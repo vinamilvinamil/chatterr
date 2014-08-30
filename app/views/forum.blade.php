de4dd027517bf9aaafa00f902ffc4a7d1e142615
MHanslo <mikehanslo@gmail.com>
2014-08-30 16:37:11 +0200

    Forum to table refactor

--- app/views/forum.blade.php
+++ app/views/forum.blade.php
@@ -15,52 +15,35 @@
 			<li><a href="#">{{ $category -> name }}</a></li>
 			@endforeach	
 		</ul>
-	</div>	
-	<button type="button" class="btn btn-primary">Normal</button>	
+	</div>		
 	<button type="button" class="btn btn-primary">Hot</button>
-	<a href="{{ url('newTopic') }}"><button type="button" class="btn btn-default">New Topic</button></a>		
+	<a href="{{ url('newTopic') }}"><button type="button" class="btn btn-default"><span class="fa fa-pencil glyph"></span>New Topic</button></a>		
 </div>
 
-<div class="container forum-headings">
-	<div class="row">
-		<h4>
-			<div class="col-md-4">Topics</div>
-			<div class="col-md-1">Category</div>
-			<div class="col-md-2">Users</div>
-			<div class="col-md-1">Posts</div>
-			<div class="col-md-1">Likes</div>
-			<div class="col-md-1">Views</div>
-			<div class="col-md-2">Activity</div>
-		</h4>
-	</div>
-</div>
-
-<div class="container forum-headings">
-	@foreach($topics as $topic)
-	<div class="row">
-		<div class="col-md-4">
-			<a href="{{ url('topic', $topic -> id) }}">{{ $topic -> title }}</a>
-		</div>
-		<div class="col-md-1">
-			{{ $topic -> category -> name }}
-		</div>
-		<div class="col-md-2">
-			{{ $topic -> user -> username }}
-		</div>
-		<div class="col-md-1">
-			{{ $topic -> posts -> count() }}
-		</div>
-		<div class="col-md-1">
-			{{ $topic -> likes }}
-		</div>
-		<div class="col-md-1">
-			{{ $topic -> views }}
-		</div>
-		<div class="col-md-2">
-			{{ $topic -> updated_at }}
-		</div>
-	</div>
-	@endforeach	
-</div>
-
+<div class="container">
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>Topics</th>
+				<th>Category</th>
+				<th>Users</th>
+				<th>Posts</th>
+				<th>Views</th>
+				<th>Activity</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($topics as $topic)
+			<tr>
+				<td> <a href="{{ url('topic', $topic -> id) }}" class="forum-topic-title">{{ $topic -> title }}</a> </td>
+				<td> {{ $topic -> category -> name }} </td>
+				<td> {{ $topic -> user -> username }} </td>
+				<td> {{ $topic -> posts -> count() }} </td>
+				<td> {{ $topic -> views }} </td>
+				<td> {{ date("j M Y",strtotime($topic->updated_at)) }} </td>
+			</tr>
+			@endforeach	
+		</tbody>
+	</table>
+</div>	
 @stop
